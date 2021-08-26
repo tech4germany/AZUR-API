318c0b237680f8fb09969e7b5ebd8296ea8476d7
David Probst <david.probst@tum.de>
2021-08-26 14:50:51 +0200

    Updated schepers and hare_niemeyer to use dicts instead of lists

--- src/assignment.py
+++ src/assignment.py
@@ -1,16 +1,17 @@
 import numpy as np
-from typing import Mapping, Tuple
+import math
+from typing import Mapping, Tuple, Dict, List
 
 
-def dhondt(votes: Mapping[str, int], seats_available: int) -> Tuple[dict, list]:
-    """ Applies D'Hondt Method for calculating the distirbution of all seats available based on the proportions of votes
+def dhondt(votes: Mapping[str, int], seats_available: int) -> Tuple[Dict[str, int], List[str]]:
+    """ Applies D'Hondt Method for calculating the distribution of all seats available based on the proportions of votes
     :param votes: the number of votes that each party/fraction received
     :param seats_available: the total number of seats (or minutes, or rooms...) available for distribution
     :return: A Tuple containing (1) the final distribution and (2) the sequence in which these seats where distributed
     """
     
     divs = {key: 1 for (key, val) in votes.items()}  # copy dict keys but init all divisors with 1
-    seats =  {key: 0 for (key, val) in votes.items()}  # copy dict keys but init all seat counters with 0
+    seats = {key: 0 for (key, val) in votes.items()}  # copy dict keys but init all seat counters with 0
 
     assgs = []
     
@@ -19,7 +20,7 @@ def dhondt(votes: Mapping[str, int], seats_available: int) -> Tuple[dict, list]:
         vals = {key: val/divs[key] for (key, val) in votes.items()}
 
         # TODO how do we deal with multiple max values?!
-        seat_goes_to = max(vals, key=vals.get) # get key for party that gets the seat
+        seat_goes_to = max(vals, key=vals.get)  # get key for party that gets the seat
 
         assgs.append(seat_goes_to)
         seats[seat_goes_to] += 1
@@ -28,34 +29,51 @@ def dhondt(votes: Mapping[str, int], seats_available: int) -> Tuple[dict, list]:
     return seats, assgs
 
 
-def schepers(votes, seats_available):
-    
-    divs = np.array([0.5] * len(votes))
-    seats = np.array([0] * len(votes))
+def schepers(votes: Mapping[str, int], seats_available: int) -> Tuple[Dict[str, int], List[str]]:
+    """ Applies saint-lague/schepers Method for calculating the distribution of all seats available based on
+    the proportions of votes
+    :param votes: the number of votes that each party/fraction received
+    :param seats_available: the total number of seats (or minutes, or rooms...) available for distribution
+    :return: A Tuple containing (1) the final distribution and (2) the sequence in which these seats where distributed
+    """
+
+    divs = {key: 0.5 for (key, val) in votes.items()}
+    seats = {key: 0 for (key, val) in votes.items()}
     assgs = []
-    
+
+    # TODO remove code duplication between schepers and d'hondt in that loop
     for i in range(seats_available):
         
-        vals = votes / divs
-        seat_goes_to = np.argmax(vals)
-        
-        assgs.append(seat_goes_to + 1)
+        vals = {key: val/divs[key] for (key, val) in votes.items()}
+
+        # TODO how do we deal with multiple max values?!
+        seat_goes_to = max(vals, key=vals.get)  # get key for party that gets the seat
+
+        assgs.append(seat_goes_to)
         seats[seat_goes_to] += 1
         divs[seat_goes_to] += 1
         
     return seats, assgs
 
 
-def hare_niemeyer(votes, seats_available):
-    
-    props = seats_available * votes / np.sum(votes)
+def hare_niemeyer(votes: Mapping[str, int], seats_available: int) -> Dict[str, int]:
+    """ Applies Hare/Niemeyer Method for calculating the distribution of all seats available based on
+    the proportions of votes
+    :param votes: the number of votes that each party/fraction received
+    :param seats_available: the total number of seats (or minutes, or rooms...) available for distribution
+    :return: The final distribution of seats (Hare/Niemeyer produces no ordering of these seats)
+    """
+
+    votes_vals = np.array(list(votes.values()))
+    props = seats_available * votes_vals / np.sum(votes_vals)
     # print(props)
-    
+
     fulls = np.floor(props)
     # print(fulls)
     
     rest = props - fulls
-    
+    # print(rest)
+
     seats_left = seats_available - np.sum(fulls)
     # print(seats_left)
     
@@ -67,8 +85,11 @@ def hare_niemeyer(votes, seats_available):
     
     seats = fulls + gets_another_seat
     seats = seats.astype(int)
+
+    # TODO unsafe die ergebnisse einfach am ende mit den input keys zu zippen?
+    seats_labeled = dict(zip(votes.keys(), seats))
     
-    return seats
+    return seats_labeled
 
 
 def demo():
@@ -87,7 +108,7 @@ def demo():
 
 
 if __name__ == "__main__":
-    print(dhondt(
+    print(hare_niemeyer(
         {
             "cdu": 1000000,
             "grüne": 300000,

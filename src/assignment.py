0c22346b2d3d265d7608e16f74f6516215d0e139
David Probst <david.probst@tum.de>
2021-08-26 14:15:41 +0200

    Ported Dhondt to be based on dicts instead of lists (include fraction names)

--- src/assignment.py
+++ src/assignment.py
@@ -1,21 +1,30 @@
 import numpy as np
+from typing import Mapping, Tuple
 
 
-def dhondt(votes, seats_available):
+def dhondt(votes: Mapping[str, int], seats_available: int) -> Tuple[dict, list]:
+    """ Applies D'Hondt Method for calculating the distirbution of all seats available based on the proportions of votes
+    :param votes: the number of votes that each party/fraction received
+    :param seats_available: the total number of seats (or minutes, or rooms...) available for distribution
+    :return: A Tuple containing (1) the final distribution and (2) the sequence in which these seats where distributed
+    """
     
-    divs = np.array([1] * len(votes))
-    seats = np.array([0] * len(votes))
+    divs = {key: 1 for (key, val) in votes.items()}  # copy dict keys but init all divisors with 1
+    seats =  {key: 0 for (key, val) in votes.items()}  # copy dict keys but init all seat counters with 0
+
     assgs = []
     
     for i in range(seats_available):
-        
-        vals = votes / divs
-        seat_goes_to = np.argmax(vals)
-        
-        assgs.append(seat_goes_to + 1)
+        # TODO werden diese Zwischenergebnisse nicht irgendwie gerundet?
+        vals = {key: val/divs[key] for (key, val) in votes.items()}
+
+        # TODO how do we deal with multiple max values?!
+        seat_goes_to = max(vals, key=vals.get) # get key for party that gets the seat
+
+        assgs.append(seat_goes_to)
         seats[seat_goes_to] += 1
         divs[seat_goes_to] += 1
-        
+
     return seats, assgs
 
 
@@ -75,3 +84,15 @@ def demo():
 
     print('Hare-Niemeyer')
     print(str(hare_niemeyer(votes, seats)))
+
+
+if __name__ == "__main__":
+    print(dhondt(
+        {
+            "cdu": 1000000,
+            "grüne": 300000,
+            "spd": 100000,
+            "dielinke": 50000
+         },
+        25
+    ))
